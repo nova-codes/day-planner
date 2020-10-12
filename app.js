942f25b00d7a6b7759889c8db01011d0dd47eed5
Hannah Elliott <hgelliott90@gmail.com>
2020-10-11 21:47:47 -0400

    Color coding past, present, and future time periods.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 
-// color code if past, present, future
+
 // click on time block
 // user can input event
 // user can save event in local storage
@@ -12,46 +12,58 @@
 
 function dayPlanner() {
     $(document).ready(function() {
+        let m = moment();
         let dateFormat = moment().format('LLLL');
+        let currentTime = m.format('h:mma');
         let businessHours = [
             {
                 id: 'eight',
+                hour: m.hour(8),
                 time: '8:00 AM'
             },
             {
                 id: 'nine',
+                hour: m.hour(9),
                 time: '9:00 AM'
             },
             {
                 id: 'ten',
+                hour: m.hour(10),
                 time: '10:00 AM'
             },
             {
                 id: 'eleven',
+                hour: m.hour(11),
                 time: '11:00 AM'
             },
             {
                 id: 'twelve',
+                hour: m.hour(12),
                 time: '12:00 PM'
             },
             {
                 id: 'one',
+                hour: m.hour(13),
                 time: '1:00 PM'
             },
             {
                 id: 'two',
+                hour: m.hour(14),
                 time: '2:00 PM'
             },
             {
                 id: 'three',
+                hour: m.hour(15),
                 time: '3:00 PM'
             },
             {
                 id: 'four',
+                hour: m.hour(16),
                 time: '4:00 PM'
             },
             {
                 id: 'five',
+                hour: m.hour(17),
                 time: '5:00 PM'
             }
         ]
@@ -68,7 +80,7 @@ function dayPlanner() {
 
         // create timeblocks for standard business hours
         function timeBlock(){
-            $.each(businessHours, function(i, val){
+            $.each(businessHours, function(i){
                 var timeRow = $('<div>');
                     timeRow.addClass('row');
 
@@ -95,6 +107,27 @@ function dayPlanner() {
         }
 
         timeBlock(); 
+
+        // color code if past, present, future
+        function colorCode() {
+            
+            for(i = 0; i <= 23; i++) {
+                let hour = $('.hour');
+                
+                if(hour === currentTime)     {
+                    $('.time-block').addClass('present');
+                }
+                else if(hour > currentTime) {
+                    $('.time-block').addClass('past');
+                } 
+                else {
+                    $('.time-block').addClass('future');
+                }
+            }
+
+        }
+
+        colorCode();
 
     });
 
